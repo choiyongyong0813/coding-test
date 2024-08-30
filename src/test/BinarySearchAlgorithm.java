c02a8c4d9cf7232b594bd6e3b09b88b43ebbd1bc
최재용 <choiyongyong0813@gmail.com>
2024-08-31 00:31:34 +0900

    재귀방식 수정

--- src/test/BinarySearchAlgorithm.java
+++ src/test/BinarySearchAlgorithm.java
@@ -6,10 +6,10 @@ public class BinarySearchAlgorithm {
         // 정렬된 배열을 정의
         int[] sortedArray = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
         // 검색할 목표값을 설정
-        int target = 14;
+        int value = 14;
         
         // 반복방식으로 이진탐색
-        int resultIterative = binarySearchIterative(sortedArray, target);
+        int resultIterative = binarySearchIterative(sortedArray, value);
         if (resultIterative == -1) {
            
             System.out.println("반복방식: 목표값 못 찾음");
@@ -19,7 +19,7 @@ public class BinarySearchAlgorithm {
         }
 
         // 재귀방식 이진탐색
-        int resultRecursive = binarySearchRecursive(sortedArray, target, 0, sortedArray.length - 1);
+        int resultRecursive = binarySearchRecursive(sortedArray, value, 0, sortedArray.length - 1);
         if (resultRecursive == -1) {
          
             System.out.println("재귀 방식: 목표값 못 찾음");
@@ -30,15 +30,15 @@ public class BinarySearchAlgorithm {
     }
 
     // 반복문 이진탐색 메서드
-    public static int binarySearchIterative(int[] array, int target) {
+    public static int binarySearchIterative(int[] array, int value) {
     	 int left = 0;
          int right = array.length - 1;
 
          while (left <= right) {
              int mid = (left + right) / 2;  // 중간 인덱스 계산
-             if (array[mid] == target) {
+             if (array[mid] == value) {
                  return mid;  // 목표값을 찾았을 때
-             } else if (array[mid] < target) {
+             } else if (array[mid] < value) {
                  left = mid + 1;  // 목표값이 오른쪽 절반에 있는 경우
              } else {
                  right = mid - 1;  // 목표값이 왼쪽 절반에 있는 경우
@@ -52,27 +52,19 @@ public class BinarySearchAlgorithm {
 
 
     // 재귀방식 이진탐색 메서드
-    public static int binarySearchRecursive(int[] array, int target, int left, int right) {
-        // 목표 값을 찾지 못했을 경우
+    public static int binarySearchRecursive(int[] array, int value, int left, int right) {
         if (left > right) {
-            return -1;
+            return -1;  // 목표값을 찾지 못한 경우
         }
-        
-        // 현재 탐색 범위의 중간 인덱스를 계산
-        int mid = left + (right - left) / 2;
-        
-        // 중간 인덱스의 값이 목표 값과 같은지 확인
-        if (array[mid] == target) {
-            return mid; // 목표 값을 찾으면 인덱스를 반환
-        }
-        
-        // 목표값이 중간값보다 크면 왼쪽 절반을 무시하고 오른쪽 절반으로 범위를 좁혀 재귀호출을 수행
-        if (array[mid] < target) {
-            return binarySearchRecursive(array, target, mid + 1, right);
-        } 
-        // 목표값이 중간값보다 작으면 오른쪽 절반을 무시하고 왼쪽 절반으로 범위를 좁혀 재귀호출을 수행
-        else {
-            return binarySearchRecursive(array, target, left, mid - 1);
+
+        int mid = (left + right) / 2;  // 중간 인덱스 계산
+
+        if (array[mid] == value) {
+            return mid;  // 목표값을 찾았을 때
+        } else if (array[mid] < value) {
+            return binarySearchRecursive(array, value, mid + 1, right);  // 오른쪽 절반을 탐색
+        } else {
+            return binarySearchRecursive(array, value, left, mid - 1);  // 왼쪽 절반을 탐색
         }
     }
 }
