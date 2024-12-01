5c77a5b7a0dd680178f1e635c57e093652165761
최재용 <choiyongyong0813@gmail.com>
2024-12-01 20:47:46 +0900

    로또 추첨기

---
+++ src/test/teeete.java
@@ -0,0 +1,12 @@
+package test;
+
+public class teeete {
+	public static void main(String[] args) {
+		int num = 15;
+		for (int i = 1; i <= num; i++) {
+			System.out.println("안녕하세요");
+
+		
+	}
+}
+}
--- src/test/test1.java
+++ src/test/test1.java
@@ -1,25 +1,36 @@
 package test;
 
+import java.util.Arrays;
+
 public class test1 {
-	public static void main(String[] args) {
-		int num = 5;
-		int numA = 1;
-		for (int i = 1; i <= num; i++) {
-			numA *= i;
-		}
-		System.out.println(numA);
-		
-		int x = 10, y = 20;
-		int z;
-		
-		z = x;
-		x = y;
-		y = z;
-		System.out.println(x);
-		System.out.println(y);
-		
-		String a = "제발 나오라";
-		a = a.replace("나오라", "나오지마라");
-		System.out.println(a);
-	}
+    public static void main(String[] args) {
+        // 5개의 로또 번호 세트를 저장할 2차원 배열
+        int[][] lottoSets = new int[5][6];
+
+        // 5개의 세트를 생성
+        for (int i = 0; i < lottoSets.length; i++) {
+            lottoSets[i] = generateLottoNumbers(); // 각 세트 생성
+        }
+
+        // 결과 출력
+        for (int i = 0; i < lottoSets.length; i++) {
+            System.out.println("로또번호 세트 " + (i + 1) + " : " + Arrays.toString(lottoSets[i]));
+        }
+    }
+
+    // 로또 번호 한 세트를 생성하는 메서드
+    private static int[] generateLottoNumbers() {
+        int[] lotto = new int[6];
+        for (int i = 0; i < lotto.length; i++) {
+            lotto[i] = (int) (Math.random() * 45) + 1;
+            for (int j = 0; j < i; j++) { // 중복 제거
+                if (lotto[i] == lotto[j]) {
+                    i--; // 중복이면 다시 뽑음
+                    break;
+                }
+            }
+        }
+        Arrays.sort(lotto); // 오름차순 정렬
+        return lotto;
+    }
 }
